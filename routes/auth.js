46d6ff9a8099975dcfdcae6209da2fd686607d73
Arunmani21 <101567547+Arunmani21@users.noreply.github.com>
2023-03-30 12:43:07 +0530

    Update auth.js
    Routes updated

--- routes/auth.js
+++ routes/auth.js
@@ -50,104 +50,98 @@ router.post('/register', (req, res) => {
     }
 });
 
-// router.get('/private', (req, res) => {
-//     let userexists = users.find((user) => user.mnemonic === req.body.mnemonic);
-//     if(userexists){
-//         let pair =  mnemonicToKeyPair( userexists.mnemonic,"m/44'/60'/0'/0",
-//     "1234");
-//     console.log(pair.privateKey);
-//     res.send({
-//         privateKey: pair.privateKey.toString('hex'),
-//         user: userexists
-//     });    
-//     } else{
-//         res.send({
-//             message: "mnemonic Key is not valid",
-//             users
-//         });
-//     } 
-// });
-// router.post('/public', (req, res) => {
-//     console.log(req.body.privateKey, req.body.username);
-//     let provider = new HDWalletProvider(
-//    req.body.privateKey,"http://localhost:7545");
-//     console.log(provider.getAddress());
-//  let cuser = users.find( s =>{
-//         return s.username == req.body.username;
-//     });
-//     cuser.address = provider.getAddress();
-//     console.log(users);
-//     res.send({
-//     address:provider.getAddress(),
-//     user: cuser
+router.get('/private', (req, res) => {
+    let userexists = users.find((user) => user.mnemonic === req.body.mnemonic);
+    if(userexists){
+        let pair =  mnemonicToKeyPair( userexists.mnemonic,"m/44'/60'/0'/0",
+    "1234");
+    console.log(pair.privateKey);
+    res.send({
+        privateKey: pair.privateKey.toString('hex'),
+        user: userexists
+    });    
+    } else{
+        res.send({
+            message: "mnemonic Key is not valid",
+            users
+        });
+    } 
+});
+router.post('/public', (req, res) => {
+    console.log(req.body.privateKey, req.body.username);
+    let provider = new HDWalletProvider(
+   req.body.privateKey,"http://localhost:7545");
+    console.log(provider.getAddress());
+ let currentUser = users.find( s =>{
+        return s.username == req.body.username;
+    });
+    currentUser.address = provider.getAddress();
+    console.log(users);
+    res.send({
+    address:provider.getAddress(),
+    user: currentUser
     
-//     });
+    });
 
 // });
 
-// router.post("/login", (req, res) => {
-//     console.log(users);
-//     let loggedin = users.findIndex((user) => user.username === req.body.username && user.password === req.body.password);
-//     console.log({loggedin}, users[0]);
-//     if (loggedin > -1) {
-//         let token = jwt.sign(
-//             { username: req.body.username , address:users[0].address},
-//             "blockchain",
-//             { expiresIn: "1h" }
-//         );
-//         res.send({
-//             message: "loggedin successfully",
-//             token
-//         });
-//     } else {
-//         res.status(401).send({
-//             message: "unsuccess"
-//         });
-//     }
+router.post("/login", (req, res) => {
+    console.log(users);
+    let loggedin = users.findIndex((user) => user.username === req.body.username && user.password === req.body.password);
+    console.log({loggedin}, users[0]);
+    if (loggedin > -1) {
+        let token = jwt.sign(
+            { username: req.body.username , address:users[0].address},
+            "blockchain",
+            { expiresIn: "1h" }
+        );
+        res.send({
+            message: "loggedin successfully",
+            token
+        });
+    } else {
+        res.status(401).send({
+            message: "unsuccess"
+        });
+    }
 
-// });
+});
 
-// const authenticateJWT = (req, res, next) => {
-//     const authHeader = req.headers.authorization;
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
 
-//     if (authHeader) {
-//         jwt.verify(authHeader, "blockchain", (err, user) => {
-//             if (err) {
-//                 return res.sendStatus(403);
-//             }
+    if (authHeader) {
+        jwt.verify(authHeader, "blockchain", (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
 
-//             req.user = user;
-//             next();
-//         });
-//     } else {
-//         res.sendStatus(401);
-//     }
-// };
-// router.post('/mint',authenticateJWT,  async (req, res) =>{
-//     let key = await init();
-//      console.log( 'data', req.user);
-//     await key.contract.methods.mint(req.user.address,50).send({
-//         from :key.addresses[0],  
-//    }); 
-//    res.send({
-//     message : "minted"
-// });
-// }); 
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+router.post('/mint',authenticateJWT,  async (req, res) =>{
+    let key = await init();
+     console.log( 'data', req.user);
+    await key.contract.methods.mint(req.user.address,50).send({
+        from :key.addresses[0],  
+   }); 
+   res.send({
+    message : "minted"
+});
+}); 
 
-// router.get('/balance/:address', async (req, res) => {
-//     let key = await init();
-//     const result = await key.contract.methods.balanceOf(req.params.address).call();
-//     console.log(req.params.address,result);
-//     res.send({
-//         value : result
-//     }); 
-// });
+router.get('/balance/:address', async (req, res) => {
+    let key = await init();
+    const result = await key.contract.methods.balanceOf(req.params.address).call();
+    console.log(req.params.address,result);
+    res.send({
+        value : result
+    }); 
+});
 
-// router.post('/transfer',authenticateJWT, async(req, res)=> {
-//     let key = await init();
-//     const receipt =  await key.contract.methods.transfer("",50).send({
-//         from : addresses[1],
-//     });
-// });
 
  module.exports = router;
